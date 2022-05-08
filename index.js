da7b68048a497c33a48ebcad2bef94c959e3cc91
mdtomiz420 <tamizrabbi@gmail.com>
2022-05-09 05:31:42 +0600

    commirt

--- index.js
+++ index.js
@@ -15,9 +15,6 @@ app.use(cors());
 app.use(express.json());
 
 
-// user:dbuser1
-// password:xLeDtsSERnlIPrJ3
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f3xgc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 
