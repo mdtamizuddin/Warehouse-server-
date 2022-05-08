d3f15fc00abf4c0f8e693f51938fbe8ac9a3c1de
mdtomiz420 <tamizrabbi@gmail.com>
2022-05-09 00:28:52 +0600

    product end pont created

--- index.js
+++ index.js
@@ -22,44 +22,46 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         await client.connect();
-        const userCollection = client.db('Inventory').collection('product');
+        const productCollection = client.db('Inventory').collection('product');
 
         // get product
         app.get('/product', async(req, res) =>{
             const query = {};
-            const cursor = userCollection.find(query);
-            const users = await cursor.toArray();
-            res.send(users);
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
         });
 
         app.get('/product/:id', async(req, res) =>{
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
-            const result = await userCollection.findOne(query);
+            const result = await productCollection.findOne(query);
             res.send(result);
         });
 
         // POST product
         app.post('/product', async(req, res) =>{
-            const newUser = req.body;
-            console.log('adding new user', newUser);
-            const result = await userCollection.insertOne(newUser);
+            const newproduct = req.body;
+            console.log('adding new user', newproduct);
+            const result = await productCollection.insertOne(newproduct);
             res.send(result)
         });
 
         // update product
         app.put('/product/:id', async(req, res) =>{
             const id = req.params.id;
-            const updatedUser = req.body;
+            const updatedProduct = req.body;
+            console.log(updatedProduct)
             const filter = {_id: ObjectId(id)};
             const options = { upsert: true };
             const updatedDoc = {
                 $set: {
-                    name: updatedUser.name,
-                    email: updatedUser.email
+
+                  quantity: updatedProduct.quantity
+
                 }
             };
-            const result = await userCollection.updateOne(filter, updatedDoc, options);
+            const result = await productCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
 
         })
@@ -68,7 +70,7 @@ async function run(){
         app.delete('/product/:id', async(req, res) =>{
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
-            const result = await userCollection.deleteOne(query);
+            const result = await productCollection.deleteOne(query);
             res.send(result);
         })
 
